f783dd1c8ad3ad13106b943b5a21191911f4652f
中島大輔 <yrxhx513@yahoo.co.jp>
2024-08-02 19:11:38 +0900

    10章まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { getNewsList } from '@/app/_libs/microcms';
 import { TOP_NEWS_LIMIT } from '@/app/_constants';
 import NewsList from '@/app/_components/NewsList';
-import ButtonLink from './_components/ButtonLink';
+import ButtonLink from '@/app/_components/ButtonLink';
 
 export const revalidate = 60;
 
@@ -12,24 +12,30 @@ export default async function Home() {
   const data = await getNewsList({
     limit: TOP_NEWS_LIMIT,
   });
-
   return (
     <>
       <section className={styles.top}>
         <div>
           <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
           <p className={styles.description}>
-            私達は市場をリードしているグローバルテックカンパニーです。
+            私たちは市場をリードしているグローバルテックカンパニーです。
           </p>
         </div>
-        <img className={styles.bgimg} src='/img-mv.jpg' alt='' />
+        <Image
+          className={styles.bgimg}
+          src='/img-mv.jpg'
+          alt=''
+          width={4000}
+          height={1200}
+          priority
+          sizes='100vw'
+        />
       </section>
-
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
         <NewsList news={data.contents} />
         <div className={styles.newsLink}>
-          <ButtonLink href='/news'>もっと見る</ButtonLink>
+          <ButtonLink href='/news'>もっとみる</ButtonLink>
         </div>
       </section>
     </>
